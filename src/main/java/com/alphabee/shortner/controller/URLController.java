100bb58f35d30564d88af885e6bb94c36befe636
alphaBEE <ankitsmt211@gmail.com>
2023-02-27 06:46:30 +0530

    Add controller for update/remove URL methods

--- src/main/java/com/alphabee/shortner/controller/URLController.java
+++ src/main/java/com/alphabee/shortner/controller/URLController.java
@@ -6,9 +6,11 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +35,18 @@ public class URLController {
     throws IOException {
 
     urlService.fetchURL(httpServletResponse, shortURL);
+  }
+
+  @DeleteMapping("/delete/{shortURL}")
+  public void deleteURL(HttpServletResponse httpServletResponse, @PathVariable String shortURL){
+    urlService.removeURL(httpServletResponse,shortURL);
+  }
+
+  @PutMapping("/update/{shortURL}")
+  public void updateURL(HttpServletResponse httpServletResponse,@PathVariable String shortURL
+    , @RequestBody Request request){
+
+    urlService.updateURL(httpServletResponse,shortURL, request.url());
   }
 
 }
